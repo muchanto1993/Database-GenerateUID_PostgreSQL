1ad1c83d8bd9b96d5f0be366edcab0c2a8286196
Anto <m.antoberlianto1993@gmail.com>
2022-03-22 09:08:55 +0700

    Update Script - Generate API ID, API KEY, JWT KEY, JWE KEY.sql

--- Script - Generate API ID, API KEY, JWT KEY, JWE KEY.sql
+++ Script - Generate API ID, API KEY, JWT KEY, JWE KEY.sql
@@ -1,7 +1,7 @@
-select length('00117ae940fc46cbb80c1d7e4651b58b'); --32 api id
-select length('0011APIKEYSYgBh10QWLMBCLEPw6R3549rAxV7LgfRk8'); --44 api id
+select length('00117ae940fc46cbb80c1d7e4651b58b'); --32 API ID
+select length('0011APIKEYSYgBh10QWLMBCLEPw6R3549rAxV7LgfRk8'); --44 API KEY
 select length('0011JWTKEYSYgBh10QWLMBCLEPw6R3549rAxV7LgfRk8'); --44 JWT KEY
-select length('wVvnLAaioSCcRR8FJvQxzMBGNEtVTVhlw9SOIJw6SJt'); --43 JWE id
+select length('wVvnLAaioSCcRR8FJvQxzMBGNEtVTVhlw9SOIJw6SJt'); --43 JWE ID
 
 select generate_uid(32);
 select generate_uid(44);
